3e661178c8a8198e2dd1f0b6ca2a6dab26b1071a
Jp.Kha <jp.kha@live.fr>
2013-03-12 23:19:27 +0100

    Creation de la fonction motPossible. + Modification
    Modification des noms de certaines fonctions ainsi que rajoute d'une
    fonction qui permet de énumérer un mot commençant pas la lettre demandé
    s'il n'est pas trouvé !

--- src/Others/Main.java
+++ src/Others/Main.java
@@ -8,20 +8,24 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Outils outils = new Outils();
-		System.out.println(outils.leMotEstDansLaListe("litch","ListeFruits.txt"));
-		System.out.println(outils.motPossible("litchE", "litchi"));
-		System.out.println(outils.motPossible("litehE", "litchi"));
-		System.out.println(outils.motPossible("litchiazejaivefovb", "litchi"));
-		System.out.println(outils.motPossible("azejaivefovb", "litchi"));
-		
 		Questions question = new Questions();
-		System.out.println(question.choisirListe());
-		System.out.println(question.choisirListe());
-		System.out.println(question.choisirLettre());
-		System.out.println(question.choisirLettre());
-		System.out.println(question.laQuestionEst());
+		Outils outils = new Outils();
+		System.out.println("Le mot est il dans la liste ?"); 
+		char lettre = question.choisirLettre();
+		System.out.println("La lettre choisi est "+lettre);
+		System.out.println(outils.leMotEstDansLaListe("litchazez","ListeFruits.txt", lettre));
+//		System.out.println("test de rechercheDuMotDansLaListe");
+//		System.out.println(outils.rechercheDuMotDansLaListe("cacaboudion","ListeFruits.txt", 'C'));
+//		System.out.println(outils.motMalTape("azejaivefovb", "litchi"));
+		
+
+//		System.out.println(question.choisirListe());
+//		System.out.println(question.choisirListe());
+//		System.out.println(question.choisirLettre());
+//		System.out.println(question.choisirLettre());
+//		System.out.println(question.laQuestionEst());
+		
+//		System.out.println(outils.MotPossible("ListeFruits.txt", lettre));
 	}
 
 }
--- src/Others/Outils.java
+++ src/Others/Outils.java
@@ -6,77 +6,102 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-
+import java.lang.Character;
 
 public class Outils {
-	
+
 	private BufferedReader br;
 
-	public Outils(){
-	
+	public Outils() {
+
 	}
-	
-	public boolean leMotEstDansLaListe(String mot,String texte){
-		String fichier =texte;
-		//lecture du fichier texte	
-				try{
-					InputStream ips=new FileInputStream(fichier); 
-					InputStreamReader ipsr=new InputStreamReader(ips);
-					br = new BufferedReader(ipsr);
-					String ligne;
-					String word = mot.toUpperCase();
-					while ((ligne=br.readLine())!=null){							
-						String ligneMaj=ligne.toUpperCase();
-						if( motPossible(word,ligneMaj)){								
-							return true;
-						}
-					}					
-					br.close(); 
-				}		
-				catch (Exception e){
-					System.out.println(e.toString());
+
+	public String leMotEstDansLaListe(String mot, String texte, char lettre) {
+		if (rechercheDuMotDansLaListe(mot, texte, lettre)) {
+			return "Le mot est dans la liste +1 ! ";
+		} else {
+			return " Le mot n'est pas bon mais voici un exemple de mot possible :"+ MotPossible(texte, lettre);
+		}
+	}
+
+	public boolean rechercheDuMotDansLaListe(String mot, String texte, char lettre) {
+		String fichier = texte;
+		if (Character.toUpperCase(mot.charAt(0)) != Character.toUpperCase(lettre)) {
+			return false;
+		}
+		// lecture du fichier texte
+		try {
+			InputStream ips = new FileInputStream(fichier);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			br = new BufferedReader(ipsr);
+			String ligne;
+			String motMaj = mot.toUpperCase();
+			while ((ligne = br.readLine()) != null) {
+				String ligneMaj = ligne.toUpperCase();
+				if (motMalTape(motMaj, ligneMaj)) {
+					return true;
 				}
-				return false;
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return false;
 	}
 
 	// Gere la possibilité que la personne est mal tapé
-	public boolean motPossible(String motTape,String motReference){
-		/*if(motTape.length()!= motReference.length()){
-			return false;
-		}
-		*/
+	public boolean motMalTape(String motTape, String motReference) {
+		/*
+		 * if(motTape.length()!= motReference.length()){ return false; }
+		 */
 		int nbReference;
-		if(motReference.length()>motTape.length()){
-			nbReference=motTape.length();
-		}
-		else{
-			nbReference=motReference.length();
+		if (motReference.length() > motTape.length()) {
+			nbReference = motTape.length();
+		} else {
+			nbReference = motReference.length();
 		}
 		int nbCharJuste = 0;
-		for(int i = 0 ; i<nbReference ; i++){
-			if(String.valueOf(motTape.charAt(i)).compareTo(String.valueOf(motReference.charAt(i)))==0){
+		for (int i = 0; i < nbReference; i++) {
+			if (String.valueOf(motTape.charAt(i)).compareTo(
+					String.valueOf(motReference.charAt(i))) == 0) {
 				nbCharJuste++;
 			}
 		}
-		int nbTemp=nbReference-nbCharJuste;
-		if(nbTemp==0 || nbTemp==1){
+		int nbTemp = nbReference - nbCharJuste;
+		if (nbTemp == 0 || nbTemp == 1) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
-	public int nbLigneTexte(String texte) throws IOException{
+
+	public String MotPossible(String texte, char lettre) {
+		try {
+			InputStream ips = new FileInputStream(texte);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			br = new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne = br.readLine()) != null) {
+				if (ligne.charAt(0) == lettre) {
+					return ligne;
+				}
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		return "le mot n'existe pas en : " + lettre;
+	}
+
+	public int nbLigneTexte(String texte) throws IOException {
 		int count = 0;
 		String str;
 		FileInputStream fis = new FileInputStream(texte);
-		LineNumberReader l = new LineNumberReader(br);      
+		LineNumberReader l = new LineNumberReader(br);
 		new BufferedReader(new InputStreamReader(fis));
-		   while ((str=l.readLine())!=null){ 
-		      count = l.getLineNumber();
-		   }
-		  return count;
+		while ((str = l.readLine()) != null) {
+			count = l.getLineNumber();
+		}
+		return count;
 	}
 }
-
